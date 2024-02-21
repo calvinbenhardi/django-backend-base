ff15138851846c12fe54552211ab5e7a2a1def69
calvinbenhardi <calvinbenhardi@gmail.com>
2024-02-22 01:13:29 +0700

    chore: add STATIC_ROOT, MEDIA_URL & MEDIA_ROOT

--- config/settings.py
+++ config/settings.py
@@ -75,6 +75,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR + "/src/static",
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 AUTH_USER_MODEL = "src.User"
 PASSWORD_HASHERS = [
